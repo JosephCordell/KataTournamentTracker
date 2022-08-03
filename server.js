49328d5a07b11e73ad1a4533be0b565e636e6e6b
Joseph Cordell <joseph@jcordell.com>
2022-08-03 16:37:23 -0700

    Removed app.get*

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(
 
 app.use(routes)
 
-app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+// app.get('*',(req,res) => {
+//     res.sendFile(path.join(__dirname, './client/build/index.html'))
+// })
 
 
 sequelize.sync({ force: false }).then(() => {

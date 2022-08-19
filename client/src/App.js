6d06f1c5782798e75fdf5a2d16702bd396339745
Joseph Cordell <joseph@jcordell.com>
2022-08-19 11:14:51 -0700

    Creating Header

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './views/Homepage'
+import Header from './components/Header';
+import ScoreCalculator from './components/ScoreCalculator';
 
 function App() {
 
   return (
 
     <BrowserRouter> 
+      <Header/>
       <Routes>
         <Route path = '/' element={<Homepage />} />
+        <Route path = '/calculator' element={<ScoreCalculator />} />
       </Routes>
     </BrowserRouter>
     );

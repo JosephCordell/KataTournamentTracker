adbfaae3a2f4474fc5a2e82801a7565fa9f1d20b
Joseph Cordell <joseph@jcordell.com>
2022-09-11 11:40:38 -0700

    Updated message

--- client/src/views/Homepage.js
+++ client/src/views/Homepage.js
@@ -7,7 +7,7 @@ export default function Homepage() {
         <div>
             <h1 className='fairwoodTitle'> Fairwood Martial Arts Tournament Tracker</h1>
 
-            I don't know what to put here right now
+            The start of a tracker for the kata tournament 
 
         </div>
     )
